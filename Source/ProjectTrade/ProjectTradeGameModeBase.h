a9eb43097ef4809ae38889e323cf51c21feed990
YamSaeng <tyhg56@naver.com>
2019-08-12 19:17:11 +0900

    Init

---
+++ Source/ProjectTrade/ProjectTradeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectTradeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTRADE_API AProjectTradeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
